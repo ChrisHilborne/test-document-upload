23e5a868d5362734123716a99c651927dee35f9c
ChrisHilborne <chris.hilborne@gmail.com>
2023-09-08 22:32:20 +0200

    Created openAPI definition and used open-api-generator-cli to generate method stubs for controller

---
+++ server/src/main/java/com/zerocopy/test/documentupload/api/v1/DocApiController.java
@@ -0,0 +1,45 @@
+package com.zerocopy.test.documentupload.api.v1;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import javax.annotation.Generated;
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-09-08T22:23:18.985393663+02:00[Europe/Madrid]")
+@Controller
+@RequestMapping("${openapi.documentManagement.base-path:}")
+public class DocApiController implements DocApi {
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public DocApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
