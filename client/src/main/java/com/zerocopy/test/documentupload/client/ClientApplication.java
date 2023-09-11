292063ee976ebdc8682b9e266f57dab13e47242c
ChrisHilborne <chris.hilborne@gmail.com>
2023-09-11 11:36:45 +0200

    Created program structure for Spring MVC client

---
+++ client/src/main/java/com/zerocopy/test/documentupload/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package com.zerocopy.test.documentupload.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
